8af6ed32f4a7b08ae2e407f8d745978f09ce4d2c
Dean Pierce <pierce403@gmail.com>
2025-11-15 13:32:38 -0800

    Use prisma-client-js output to fix engine path on Vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 type GlobalWithPrisma = typeof globalThis & {
   prisma?: PrismaClient;
